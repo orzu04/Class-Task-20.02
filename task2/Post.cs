ee0833d10b254e34ef9a9fc82f899b7a9eb0d075
Panakov Orzu <panakovorzu33@gmail.com>
2024-02-21 17:46:19 +0500

    first commit

---
+++ task2/Post.cs
@@ -0,0 +1,29 @@
+﻿namespace task2;
+
+public class Post
+{
+
+public string Text;
+public int Likes;
+public List<string> Coments;
+
+  public void SetPost(string text)
+  {
+    Text = text;
+  }
+
+  public void AddLike()
+  {
+    Likes++;
+  }
+  public void AddComent(string comment)
+  {
+    Coments.Add(comment);
+  }
+  public string GetInfo()
+  {
+    return $"Text: {Text} \n Likes: {Likes} \n Comments: {Coments} ";
+  }
+ 
+
+}
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+using task2;
+
+var post = new Post();
+post.SetPost("Norak");
+post.AddLike();
+post.AddLike();
+
+post.AddComent("Sozi bad");
+post.AddComent($"Be mo ha");
+
+System.Console.WriteLine(post.GetInfo());
+
+System.Console.WriteLine($"\n Coments {post.Coments.Count}");
+
+int i=1;
+foreach (var comment in post.Coments)
+{
+    System.Console.WriteLine($"{i} {comment}");
+    i++;
+}
